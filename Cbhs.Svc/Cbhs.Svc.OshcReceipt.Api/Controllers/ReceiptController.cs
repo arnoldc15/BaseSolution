03f2914573eb969987a1e36904b2096fe03a0150
arnoldc15 <50857177+arnoldc15@users.noreply.github.com>
2019-07-29 12:49:19 +1000

    Add files via upload

---
+++ Cbhs.Svc/Cbhs.Svc.OshcReceipt.Api/Controllers/ReceiptController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using AttributeRouting.Web.Http;
+using Cbhs.Svc.OshcReceipt.Application.Queries.GetPremiumByMemberId;
+using Cbhs.Svc.OshcReceipt.Application.Queries.GetQuote;
+using Cbhs.Svc.OshcReceipt.Application.Queries.GetReceiptByReceiptId;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cbhs.Svc.OshcReceipt.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceiptController : BaseController
+    {
+        //PLEASE NOTE URI ARE NOT RESOURCE BASE. This is in preparation to Azure Functions
+        [HttpGet("GetQuote")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<QuoteViewModel>> GetQuote([FromQuery] decimal discountPercentage,
+            [FromQuery] decimal rebatePercentage,
+            [FromQuery] decimal productCost,
+            [FromQuery] DateTime startDate,
+            [FromQuery] DateTime endDate)
+        {
+            return Ok(await Mediator.Send(new GetQuoteQuery
+            {
+                DiscountPercentage = discountPercentage,
+                RebatePercentage = rebatePercentage,
+                ProductCost = productCost,
+                StartDate = startDate,
+                EndDate = endDate
+            }));
+        }
+
+        [HttpGet("GetReceiptByReceiptId/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<QuoteViewModel>> GetReceiptByReceiptId(int id)
+        {
+            return Ok(await Mediator.Send(new GetReceiptByReceiptIdQuery
+            {
+                ReceiptId = id
+            }));
+        }
+
+        [HttpGet("CalculatePremium/{memberId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<QuoteViewModel>> CalculatePremium(int memberId)
+        {
+            return Ok(await Mediator.Send(new GetPremiumByMemberIdQuery
+            {
+                memberId = memberId
+            }));
+        }
+    }
+}
---
+++ Cbhs.Svc/Cbhs.Svc.OshcReceipt.Api/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+using Cbhs.Svc.OshcReceipt.Application.Interfaces;
+using Cbhs.Svc.OshcReceipt.Application.Interfaces.Infrastructure;
+using Cbhs.Svc.OshcReceipt.Application.Queries.GetQuote;
+using Cbhs.Svc.OshcReceipt.Infrastructure;
+using Cbhs.Svc.OshcReceipt.Persistence;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cbhs.Svc.OshcReceipt.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Add MediatR
+            services.AddMediatR(typeof(GetQuoteQueryHandler).GetTypeInfo().Assembly);
+
+            // Add DBContext
+            services.AddDbContext<IReceiptDbContext, ReceiptDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ParagonDatabase")));
+
+            // Add framework services.
+            services.AddHttpClient<IRebateService, RebateService>();
+            services.AddHttpClient<IProductService, ProductService>();
+            services.AddHttpClient<IDiscountService, DiscountService>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseHsts();
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
